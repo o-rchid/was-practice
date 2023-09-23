835f66846f19ccc44cd7c245959005ca53ed61a0
o-rchid <ozzve35@gmail.com>
2023-09-23 19:51:42 +0900

    Thread 처리

---
+++ src/main/java/org/example/ClientRequestHandler.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import org.example.arithmetic_calculate.domain.Calculator;
+import org.example.arithmetic_calculate.domain.PositiveNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class ClientRequestHandler implements Runnable {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClientRequestHandler.class);
+
+    private final Socket clientSocket;
+
+    public ClientRequestHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+
+        logger.info("[ClientRequestHandler] new client {} started.", Thread.currentThread().getName());
+
+        try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            DataOutputStream dos = new DataOutputStream(out);
+
+            HttpRequest httpRequest = new HttpRequest(br);
+
+            if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
+                QueryStrings queryString = httpRequest.getQueryStrings();
+
+                int operand1 = Integer.parseInt(queryString.getValue("operand1"));
+                String operator = queryString.getValue("operator");
+                int operand2 = Integer.parseInt(queryString.getValue("operand2"));
+
+                int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+                byte[] body = String.valueOf(result).getBytes();
+
+                HttpResponse response = new HttpResponse(dos);
+                response.response200Header("application/json", body.length);
+                response.responseBody(body);
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
